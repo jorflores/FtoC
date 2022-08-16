29627c1451b99966a604018f93dc95a240a2c5e9
Jorge Flores <flores.jorgeluis@gmail.com>
2022-08-15 20:10:59 -0500

    first commit

---
+++ app/src/main/java/com/example/ftoc/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.ftoc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.ftoc.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 2 Click . Registrar el click
+
+        binding.btnConvertir.setOnClickListener{
+            // 3 Recuperar el input
+
+         val fahrenheit = binding.editTextF.text.toString().toDouble()
+            val celsius = (fahrenheit-32) * (5.0/9.0)
+            binding.editTextC.setText(String.format("%.1f",celsius))
+
+           val selected =  binding.radioGroupTemp.checkedRadioButtonId
+
+            val valor = when (selected) {
+                R.id.radioButton_C  -> "Celsius"
+                else -> "Fahrenheit"
+
+            }
+
+            val switchVal = binding.switch1.isChecked
+
+            Toast.makeText(this,valor,Toast.LENGTH_SHORT).show()
+
+            Toast.makeText(this,switchVal.toString(),Toast.LENGTH_SHORT).show()
+
+
+
+
+
+        }
+    }
+}
